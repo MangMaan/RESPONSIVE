4efb60321c7138f53f3d6f97cec0844a775c25f1
Myeongjin Kim <91296414+MangMaan@users.noreply.github.com>
2021-11-02 00:13:58 +0900

    20211102
    반응형2

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+    $('#banner').slick({
+        autoplay: true,
+        Infinity: true,
+        arrows: false,
+        dots: true,
+    });
+
+
+    $('.items').slick({
+        autoplay: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        dots: false,
+        arrows: true,
+    });
+    $('.visual1').slick({
+        autoplay: true,
+        dots: true,
+        arrows: false,
+        infinite: true,
+    })
+    $('.acne,.acne1,.acne2,.acne3').slick({
+        autoplay: true,
+        dots: false,
+        arrows: false,
+        infinite: true
+    })
+
+
+    $('.gnb').hide();
+    $('.ham_wrap').click(function () {
+        $('.gnb').fadeIn();
+    })
+    $('.close_wrap').click(function () {
+        $('.gnb').hide();
+    });
+
+
+
+})/* end */
