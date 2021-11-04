ad82c39810e1ea401d25373d98d8d14664b7a31f
Myeongjin Kim <91296414+MangMaan@users.noreply.github.com>
2021-11-04 17:32:08 +0900

    20211104
    반응형 모바일 추가

--- js/main.js
+++ js/main.js
@@ -5,6 +5,19 @@ $(document).ready(function () {
         Infinity: true,
         arrows: false,
         dots: true,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true,
+                    autoplay: true,
+                    arrows: true,
+                }
+            }
+        ]
     });
 
 
